d6f9e729d6f500306177d8e9500b81e068057886
Michael Whatcott <mike@smarty.com>
2025-10-24 16:20:36 -0600

    Full test suite for delete task example route.

---
+++ example/internal/http/delete_task_test.go
@@ -0,0 +1,74 @@
+package http
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/smarty/gunit"
+	"github.com/smarty/gunit/assert/should"
+	"github.com/smarty/scuter"
+	"github.com/smarty/scuter/example/internal/app"
+)
+
+func TestDeleteTaskFixture(t *testing.T) {
+	gunit.Run(new(DeleteTaskFixture), t)
+}
+
+type DeleteTaskFixture struct {
+	*gunit.Fixture
+	*HTTPFixture
+}
+
+func (this *DeleteTaskFixture) Setup() {
+	this.HTTPFixture = &HTTPFixture{Fixture: this.Fixture}
+	this.HTTPFixture.Setup()
+}
+
+func (this *DeleteTaskFixture) TestInvalidID() {
+	this.assertFullHTTP(http.MethodDelete, "/tasks",
+		scuter.Request.With(
+			scuter.Request.Query("id", "INVALID"),
+		),
+		scuter.Response.With(
+			scuter.Response.StatusCode(http.StatusBadRequest),
+			scuter.Response.JSONError(scuter.Error{
+				Fields:  []string{"id"},
+				Name:    "invalid-id",
+				Message: "The id was invalid or not supplied.",
+			}),
+		),
+	)
+}
+func (this *DeleteTaskFixture) TestUnrecognizedApplicationError() {
+	this.app = func(v any) { v.(*app.DeleteTaskCommand).Result.Error = errors.New("boink") }
+	this.assertFullHTTP(http.MethodDelete, "/tasks",
+		scuter.Request.Query("id", "42"),
+		scuter.Response.With(
+			scuter.Response.StatusCode(http.StatusInternalServerError),
+			scuter.Response.JSONError(scuter.Error{
+				ID:      54321,
+				Name:    "internal-server-error",
+				Message: "Internal Server Error",
+			}),
+		),
+	)
+}
+func (this *DeleteTaskFixture) TestTaskNotFound() {
+	this.app = func(v any) { v.(*app.DeleteTaskCommand).Result.Error = app.ErrTaskNotFound }
+	this.assertFullHTTP(http.MethodDelete, "/tasks",
+		scuter.Request.Query("id", "42"),
+		scuter.Response.StatusCode(http.StatusOK),
+	)
+}
+func (this *DeleteTaskFixture) TestSuccess() {
+	this.app = func(v any) {
+		command := v.(*app.DeleteTaskCommand)
+		this.So(command.ID, should.Equal, 42)
+		command.Result.Error = nil
+	}
+	this.assertFullHTTP(http.MethodDelete, "/tasks",
+		scuter.Request.Query("id", "42"),
+		scuter.Response.StatusCode(http.StatusOK),
+	)
+}
