fe06ddef92b61009b23efd1d54735e1039ea4644
Michael Whatcott <mike@smarty.com>
2025-11-05 15:01:33 -0700

    wrap

--- errors.go
+++ errors.go
@@ -2,17 +2,21 @@ package scuter
 
 // Error represents some kind of problem, most likely with the calling HTTP request.
 type Error struct {
-	// Fields indicates the exact location(s) of the errors including the part of the HTTP request itself this is
-	// invalid. Valid field prefixes include "path", "query", "header", "form", and "body".
+	// Fields indicates the exact location(s) of the errors including the part of
+	// the HTTP request itself this is invalid. Valid field prefixes include
+	// "path", "query", "header", "form", and "body".
 	Fields []string `json:"fields,omitempty"`
 
-	// ID represents the unique, numeric contractual identifier that can be used to associate this error with a particular front-end error message, if any.
+	// ID represents the unique, numeric contractual identifier that can be used to
+	// associate this error with a particular front-end error message, if any.
 	ID int `json:"id,omitempty"`
 
-	// Name represents the unique string-based, contractual value that can be used to associate this error with a particular front-end error message, if any.
+	// Name represents the unique string-based, contractual value that can be used to
+	// associate this error with a particular front-end error message, if any.
 	Name string `json:"name,omitempty"`
 
-	// Message represents a friendly, user-facing message to indicate why there was a problem with the input.
+	// Message represents a friendly, user-facing message to indicate why there was a
+	// problem with the input.
 	Message string `json:"message,omitempty"`
 }
 
